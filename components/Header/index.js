7add082eec532ed075d8f27739fae89312194833
Matt Romanes <mattromanes@gmail.com>
2021-08-03 21:57:44 +1200

    Create new package and source files
    These new package and source files will form the header at the top of the screen on the app.

---
+++ components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+import styles from './styles'
+
+const whatever = (props) => {
+    return (
+        <View style={styles.container}>
+            <Text>notJust a component</Text>
+        </View>
+    );
+};
+
+export default whatever;
