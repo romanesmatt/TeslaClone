ddbd03f0dacc9714ad3d1533f9037da381f05ad7
Matt Romanes <mattromanes@gmail.com>
2021-08-03 13:53:09 +1200

    Add snap to view feature for FlatList
    The UI should snap to view when a picture emerges from either the top or bottom of screen; added four properties to allow this to happen.

--- components/CarsList/index.js
+++ components/CarsList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, Dimensions } from 'react-native';
 import styles from './styles';
 import CarItem from '../CarItem';
 import cars from './cars'
@@ -10,6 +10,10 @@ const CarsList = (props) => {
             <FlatList 
                 data={cars}
                 renderItem={({item}) => <CarItem car={item} /> }
+                showsVerticalScrollIndicator={false}
+                snapToAlignment={'start'}
+                decelerationRate={'fast'}
+                snapToInterval={Dimensions.get('window').height}
             />
         </View>
     );
