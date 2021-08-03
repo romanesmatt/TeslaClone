5b7ff84cc7f230d15a4462dba5a87bb49a9a1ad7
Matt Romanes <mattromanes@gmail.com>
2021-08-04 11:52:44 +1200

    Render header of mobile app
    Added logo and 'hamburger menu' at the top of the screen

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
 import CarsList from './components/CarsList';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      {/* Component */}
+      {/* Components */}
+      <Header />
       <CarsList />
 
       <StatusBar style="auto" />
--- components/Header/index.js
+++ components/Header/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { View, Image } from 'react-native';
 import styles from './styles'
 
-const whatever = (props) => {
+const Header = (props) => {
     return (
         <View style={styles.container}>
-            <Text>notJust a component</Text>
+            <Image style={styles.logo} source={require('../../assets/images/logo.png')} />
+            <Image style={styles.logo} source={require('../../assets/images/menu.png')} />
         </View>
     );
 };
 
-export default whatever;
+export default Header;
